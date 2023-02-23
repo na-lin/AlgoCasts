fdc1b6a27e1485460af2359623c69f307dd9a4df
Na Lin <lna483018@gmail.com>
2023-02-22 22:25:19 -0500

    change order

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,31 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// 0 -> 0
+// reverse integer -> reverse string
+// integer is positive or negative -> Math.sign()
+// start with 0 ->
+
+// function reverseInt(n) {
+//   let reversed = n;
+
+//   if (Math.sign(n)) {
+//     reversed = n.toString().split("").reverse()
+//   } else {
+//     reversed = (n * -1).toString().split("").reverse()
+//   }
+
+//   // ? 500 -> 5 ???
+
+//   return Math.sign(n) ? parseInt(reversed) : parseInt(reversed) * -1;
+// }
+
+function reverseInt(n) {
+  // parseInt('0011-') -> 11
+  let reversed = n.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(n);
+}
+
+console.log(reverseInt(1));
 
 module.exports = reverseInt;
